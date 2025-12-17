fdf53e77f6aac80bba858783a25eb58d4cfdfab4
Octavio Ortega <octaviortegan@hotmail.com>
2025-12-17 10:30:36 -0600

    Gun hit effect / Gun animations

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,9 +4,13 @@ public class Gun : MonoBehaviour
 {
     [SerializeField] Transform muzzle;
     [SerializeField] GameObject muzzleFlashEffect;
+    [SerializeField] GameObject hitEffect;
+    Animator animator;
 
     public void Fire(float damage, float range)
     {
+        animator.Play("Gun Shoot", 0, 0f);
+
         Instantiate(muzzleFlashEffect, muzzle);
 
         RaycastHit hit;
@@ -19,9 +23,16 @@ public class Gun : MonoBehaviour
             return;
         }
 
+        Instantiate(hitEffect, hit.point, Quaternion.identity);
+
         if (hit.collider.TryGetComponent(out Health enemyHealth))
         {
             enemyHealth.TakeDamage(damage);
         }
+    }
+
+    void Awake()
+    {
+        animator = GetComponentInParent<Animator>();
     }
 }
