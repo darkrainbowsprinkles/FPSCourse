f86e85fb8696d9bca60e61d75c38e84b017e5a40
Octavio Ortega <octaviortegan@hotmail.com>
2025-12-16 23:27:25 -0600

    Muzzle flash effect

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    [SerializeField] Transform muzzle;
+    [SerializeField] GameObject muzzleFlashEffect;
+
     public void Fire(float damage, float range)
     {
+        Instantiate(muzzleFlashEffect, muzzle);
+
         RaycastHit hit;
 
         bool hitFound = Physics.Raycast(Camera.main.transform.position, 
